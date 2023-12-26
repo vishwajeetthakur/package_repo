7fdce4ab6e44909171f7ea7566ed23535d7c7cb8
Vishwajeet Thakur <vishwajeetthakur@Vishwajeets-MacBook-Pro.local>
2023-12-26 11:33:11 -0500

    Made it as package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='vishwapkg',
+    version='0.1.0',    
+    description='A example Python package',
+    url='',
+    author='Vishwajeet Thakur',
+    author_email='vishwajeeththakur@gmail.com',
+    license='BSD 2-clause',
+    packages=['vishwapkg'],
+    install_requires=['numpy',
+                      'pandas',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
