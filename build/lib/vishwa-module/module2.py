b7ecdd634374d2888b1462b587aded14cc703e8f
vishwajeetthakur <43552185+vishwajeetthakur@users.noreply.github.com>
2023-12-26 13:12:27 -0500

    Delete build/lib directory

--- build/lib/vishwa-module/module2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def get_pandas_data():
-    mydataset = {
-    'cars': ["BMW", "Volvo", "Ford"],
-    'passings': [3, 7, 2]
-    }
-    return pd.DataFrame(mydataset)
--- build/lib/vishwapkg/module1.py
+++
@@ -1,7 +0,0 @@
-import numpy  as np 
-import pandas as pd 
-
-def get_array_val():
-    arr = np.array([1, 2, 3, 4, 5])
-    return type(arr)
-
