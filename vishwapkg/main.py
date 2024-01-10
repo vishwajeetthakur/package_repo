88935592133d7ba2f5fa5b47fb969f89d331b1f1
Vishwajeet Thakur <vishwajeetthakur@Vishwajeets-MacBook-Pro.local>
2024-01-10 10:32:35 -0500

    Logic: create folder and file

--- vishwapkg/main.py
+++ vishwapkg/main.py
@@ -1,21 +1,28 @@
-import numpy  as np 
-import pandas as pd 
+# import numpy  as np 
+# import pandas as pd 
 import os
-
-# Load environment variables from .env file
-from dotenv import load_dotenv
-load_dotenv()
-
-# Access environment variables
-USERNAME = os.getenv("Vishwa")
-PASSWORD = os.getenv("Psswd")
-
-from test import abc, pw
+import os
 
 class Demo:
     def __init__(self) -> None:
-        self.USERNAME = abc 
-        self.PASSWORD = pw
+        pass
 
-    def get_all(self):
-        return (self.USERNAME, self.PASSWORD)
+    def make_folder_and_file(self):
+        folder_name = 'my_folder'
+        file_name = 'my_file.txt'
+        # Create the folder if it doesn't already exist
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        # Create and open the file in write mode
+        file_path = os.path.join(folder_name, file_name)
+        with open(file_path, 'w') as file:
+            file.write("This is a test file.")
+
+        print(f"Folder '{folder_name}' and file '{file_name}' created successfully.")
+
+obj = Demo()
+
+obj.make_folder_and_file()
+
+        
